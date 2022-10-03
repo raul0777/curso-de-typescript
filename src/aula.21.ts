b29384c8f838de1a4f824ace3bcb729bd09505f9
raul0777 <raul.gome44@gmail.com>
2022-10-03 13:37:25 -0300

    Usando this como tipo

--- src/aula.21.ts
+++ src/aula.21.ts
@@ -51,21 +51,81 @@
 // console.log(traduzirCore('roxo', coresObj));
 
 // /* ################# Aula 39 ################# */
-type Veiculo = {
-  marca: string;
-  ano: number;
-};
+// type Veiculo = {
+//   marca: string;
+//   ano: number;
+// };
 
-type Car = {
-  brand: Veiculo['marca'];
-  year: Veiculo['ano'];
-  name: string;
-};
+// type Car = {
+//   brand: Veiculo['marca'];
+//   year: Veiculo['ano'];
+//   name: string;
+// };
 
-const carro: Car = {
-  brand: 'Ford',
-  year: 2020,
-  name: 'ka',
-};
+// const carro: Car = {
+//   brand: 'Ford',
+//   year: 2020,
+//   name: 'ka',
+// };
 
-console.log(carro);
+// console.log(carro);
+
+// /* ################# Aula 40 ################# */
+export class Calculadora {
+  constructor(public numero: number) {}
+
+  add(n: number): this {
+    this.numero += n;
+    return this;
+  }
+
+  sub(n: number): this {
+    this.numero -= n;
+    return this;
+  }
+
+  div(n: number): this {
+    this.numero /= n;
+    return this;
+  }
+
+  mul(n: number): this {
+    this.numero *= n;
+    return this;
+  }
+}
+
+export class SubCalculadora extends Calculadora {
+  pow(n: number): this {
+    this.numero **= n;
+    return this;
+  }
+}
+
+const calculadora = new SubCalculadora(10);
+calculadora.add(5).mul(2).div(2).sub(5).pow(2);
+console.log(calculadora);
+
+//Builder -GoF
+
+export class RequestBuilder {
+  private method: 'get' | 'post' | null = null;
+  private url: string | null = null;
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setUrl(url: string): this {
+    this.url = url;
+    return this;
+  }
+
+  send(): void {
+    console.log(`Enviando dados via ${this.method} para ${this.url}`);
+  }
+}
+
+const request = new RequestBuilder(); //Builder
+request.setUrl('http://www.google.com').setMethod('post').send();

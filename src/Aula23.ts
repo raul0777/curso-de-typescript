1f5bab2e2e4759d17d8e0d59bdf7e4731339408e
raul0777 <raul.gome44@gmail.com>
2022-10-05 15:01:08 -0300

    Decoradores demétodo(method decorator)

--- src/Aula23.ts
+++ src/Aula23.ts
@@ -54,42 +54,90 @@
 // console.log(animal);
 
 // /* ################# Aula 53 ################# */
-interface Constructor {
-  new (...args: any[]): any;
-}
-function inverteNomeCor(param1: string, param2: string) {
-  return function <T extends Constructor>(target: T): T {
-    console.log('Sou o decorador e recebi', target);
-    return class extends target {
-      cor: string;
-      nome: string;
+// interface Constructor {
+//   new (...args: any[]): any;
+// }
+// function inverteNomeCor(param1: string, param2: string) {
+//   return function <T extends Constructor>(target: T): T {
+//     console.log('Sou o decorador e recebi', target);
+//     return class extends target {
+//       cor: string;
+//       nome: string;
 
-      constructor(...args: any[]) {
-        super(...args);
-        this.nome = this.inverte(args[0]);
-        this.cor = this.inverte(args[1]);
-      }
+//       constructor(...args: any[]) {
+//         super(...args);
+//         this.nome = this.inverte(args[0]);
+//         this.cor = this.inverte(args[1]);
+//       }
 
-      inverte(valor: string): string {
-        return valor.split('').reverse().join('') + ' ' + param1 + ' ' + param2;
-      }
-    };
+//       inverte(valor: string): string {
+//         return valor.split('').reverse().join('') + ' ' + param1 + ' ' + param2;
+//       }
+//     };
+//   };
+// }
+
+// function outroDecorador(param1: string) {
+//   return function (target: Constructor) {
+//     console.log('Sou o outro decorador' + param1);
+//     return target;
+//   };
+// }
+// @outroDecorador('O parâmetro do outro decorador') //2
+// @inverteNomeCor('Outra coisa', 'Valor2') //1
+// export class Animal {
+//   constructor(public nome: string, public cor: string) {
+//     console.log('Sou a classe');
+//   }
+// }
+
+// const animal = new Animal('romeu', 'roxo');
+// console.log(animal);
+// /* ################# Aula 55 ################# */
+function decorador(
+  classPrototype: any,
+  nomeMethod: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor | void {
+  console.log(classPrototype);
+  console.log(nomeMethod);
+  console.log(descriptor);
+  return {
+    value: function (...args: string[]) {
+      return args[0].toUpperCase();
+    },
   };
 }
 
-function outroDecorador(param1: string) {
-  return function (target: Constructor) {
-    console.log('Sou o outro decorador' + param1);
-    return target;
-  };
-}
-@outroDecorador('O parâmetro do outro decorador') //2
-@inverteNomeCor('Outra coisa', 'Valor2') //1
-export class Animal {
-  constructor(public nome: string, public cor: string) {
-    console.log('Sou a classe');
+export class UmaPessoa {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  @decorador
+  method(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
   }
 }
 
-const animal = new Animal('romeu', 'roxo');
-console.log(animal);
+const pessoa = new UmaPessoa('Raul', 'Gomes', 30);
+const method = pessoa.method('Hello world');
+console.log(method);

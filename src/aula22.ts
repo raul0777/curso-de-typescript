4125e0c93471f7d93e62abf170e3488e31d4aa54
raul0777 <raul.gome44@gmail.com>
2022-10-05 14:10:47 -0300

    Decorator de classes no TypeScript

--- src/aula22.ts
+++ src/aula22.ts
@@ -1,240 +1,240 @@
-// /* ################# Aula 43 ################# */
-// type FilterCallback<U> = (valor: U, index?: number, array?: U[]) => boolean;
+/* ################# Aula 43 ################# */
+type FilterCallback<U> = (valor: U, index?: number, array?: U[]) => boolean;
 
-// export function meuFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
-//   const novoArray = [];
+export function meuFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
+  const novoArray = [];
 
-//   for (let i = 0; i < array.length; i++) {
-//     if (callbackfn(array[i])) {
-//       novoArray.push(array[i]);
-//     }
-//   }
-//   return novoArray;
-// }
+  for (let i = 0; i < array.length; i++) {
+    if (callbackfn(array[i])) {
+      novoArray.push(array[i]);
+    }
+  }
+  return novoArray;
+}
 
-// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a'];
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a'];
 
-// const arrayFiltrandoOriginal = array.filter((value) => value < 5);
-// console.log(arrayFiltrandoOriginal);
+const arrayFiltrandoOriginal = array.filter((value) => value < 5);
+console.log(arrayFiltrandoOriginal);
 
-// const arrayFiltrando = meuFilter(array, (value) => value < 5);
-// console.log(arrayFiltrando);
+const arrayFiltrando = meuFilter(array, (value) => value < 5);
+console.log(arrayFiltrando);
 
-// /* ################# Aula 44 ################# */
-// type MeuTipo = number;
+/* ################# Aula 44 ################# */
+type MeuTipo = number;
 
-// const arrayNumber: Array<number> = [1, 2, 3, 4, 5];
-// console.log(arrayNumber);
+const arrayNumber: Array<number> = [1, 2, 3, 4, 5];
+console.log(arrayNumber);
 
-// async function promiseAsync() {
-//   return 1;
-// }
+async function promiseAsync() {
+  return 1;
+}
 
-// function minhaPromise(): Promise<MeuTipo> {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(1);
-//     }, 1000);
-//   });
-// }
+function minhaPromise(): Promise<MeuTipo> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 1000);
+  });
+}
 
-// promiseAsync().then((resultado) => console.log(resultado + 1));
-// minhaPromise().then((resultado) => console.log(resultado + 1));
+promiseAsync().then((resultado) => console.log(resultado + 1));
+minhaPromise().then((resultado) => console.log(resultado + 1));
 
-// /* ################# Aula 45 ################# */
-// interface PessoaProtocolo<T = string, U = number> {
-//   nome: T;
-//   sobrenome: T;
-//   idade: U;
-// }
+/* ################# Aula 45 ################# */
+interface PessoaProtocolo<T = string, U = number> {
+  nome: T;
+  sobrenome: T;
+  idade: U;
+}
 
-// type PessoaProtocolo2<T = string, U = number> = {
-//   nome: T;
-//   sobrenome: T;
-//   idade: U;
-// };
+type PessoaProtocolo2<T = string, U = number> = {
+  nome: T;
+  sobrenome: T;
+  idade: U;
+};
 
-// const aluno1: PessoaProtocolo2<string, number> = {
-//   nome: 'Raul',
-//   sobrenome: 'Gomes',
-//   idade: 30,
-// };
+const aluno1: PessoaProtocolo2<string, number> = {
+  nome: 'Raul',
+  sobrenome: 'Gomes',
+  idade: 30,
+};
 
-// const aluno2: PessoaProtocolo<number, number> = {
-//   nome: 123,
-//   sobrenome: 456,
-//   idade: 31,
-// };
+const aluno2: PessoaProtocolo<number, number> = {
+  nome: 123,
+  sobrenome: 456,
+  idade: 31,
+};
 
-// const aluno3: PessoaProtocolo2 = {
-//   nome: 'Romeu',
-//   sobrenome: 'Carvalho',
-//   idade: 32,
-// };
-// console.log(aluno1, aluno2, aluno3);
+const aluno3: PessoaProtocolo2 = {
+  nome: 'Romeu',
+  sobrenome: 'Carvalho',
+  idade: 32,
+};
+console.log(aluno1, aluno2, aluno3);
 
-// /* ################# Aula 46 ################# */
-// type ObterChaveFn = <O, K extends keyof O>(objeto: O, chave: K) => O[K];
+/* ################# Aula 46 ################# */
+type ObterChaveFn = <O, K extends keyof O>(objeto: O, chave: K) => O[K];
 
-// const obterChave: ObterChaveFn = (objeto, chave) => objeto[chave];
+const obterChave: ObterChaveFn = (objeto, chave) => objeto[chave];
 
-// const animal = {
-//   cor: 'Rosa',
-//   vacina: ['Vacina 1,', 'Vacina 2'],
-//   idade: 10,
-// };
+const animal = {
+  cor: 'Rosa',
+  vacina: ['Vacina 1,', 'Vacina 2'],
+  idade: 10,
+};
 
-// const vacinas = obterChave(animal, 'vacina');
-// const cor = obterChave(animal, 'cor');
+const vacinas = obterChave(animal, 'vacina');
+const cor = obterChave(animal, 'cor');
 
-// console.log(vacinas, cor, obterChave(animal, 'idade'));
+console.log(vacinas, cor, obterChave(animal, 'idade'));
 
-// /* ################# Aula 47 ################# */
-// export class Pessoa<T, U> {
-//   constructor(public nome: T, public idade: U) {}
-// }
-// export class Pilha<T> {
-//   private contador = 0;
-//   private elemento: { [k: number]: T } = {};
+/* ################# Aula 47 ################# */
+export class Pessoa<T, U> {
+  constructor(public nome: T, public idade: U) {}
+}
+export class Pilha<T> {
+  private contador = 0;
+  private elemento: { [k: number]: T } = {};
 
-//   push(elemento: T): void {
-//     this.elemento[this.contador] = elemento;
-//     this.contador++;
-//   }
+  push(elemento: T): void {
+    this.elemento[this.contador] = elemento;
+    this.contador++;
+  }
 
-//   pop(): T | void {
-//     if (this.estaVazia()) return undefined;
+  pop(): T | void {
+    if (this.estaVazia()) return undefined;
 
-//     this.contador--;
-//     const elemento = this.elemento[this.contador];
-//     delete this.elemento[this.contador];
-//     return elemento;
-//   }
+    this.contador--;
+    const elemento = this.elemento[this.contador];
+    delete this.elemento[this.contador];
+    return elemento;
+  }
 
-//   estaVazia(): boolean {
-//     return this.contador === 0;
-//   }
+  estaVazia(): boolean {
+    return this.contador === 0;
+  }
 
-//   tamanho(): number {
-//     return this.contador;
-//   }
-//   mostraPilha(): void {
-//     for (const chave in this.elemento) {
-//       console.log(this.elemento[chave]);
-//     }
-//   }
-// }
+  tamanho(): number {
+    return this.contador;
+  }
+  mostraPilha(): void {
+    for (const chave in this.elemento) {
+      console.log(this.elemento[chave]);
+    }
+  }
+}
 
-// const pilha = new Pilha<number | string>();
-// pilha.push(1);
-// pilha.push(2);
-// pilha.push(3);
-// pilha.push(4);
-// pilha.push('string');
+const pilha = new Pilha<number | string>();
+pilha.push(1);
+pilha.push(2);
+pilha.push(3);
+pilha.push(4);
+pilha.push('string');
 
-// while (!pilha.estaVazia()) {
-//   console.log(pilha.pop());
-// }
+while (!pilha.estaVazia()) {
+  console.log(pilha.pop());
+}
 
-// /* ################# Aula 48 ################# */
-// export function unirObjetos<O1, O2>(obj1: O1, obj2: O2): O1 & O2 {
-//   //return { ...obj1, ...obj2 };
-//   return Object.assign({}, obj1, obj2);
-// }
+/* ################# Aula 48 ################# */
+export function unirObjetos<O1, O2>(obj1: O1, obj2: O2): O1 & O2 {
+  //return { ...obj1, ...obj2 };
+  return Object.assign({}, obj1, obj2);
+}
 
-// const obj1 = { chave1: 'valor1' };
-// const obj2 = { chave2: 'valor2' };
-// const juntar = unirObjetos(obj1, obj2);
-// console.log(juntar);
+const obj1 = { chave1: 'valor1' };
+const obj2 = { chave2: 'valor2' };
+const juntar = unirObjetos(obj1, obj2);
+console.log(juntar);
 
-// /* ################# Aula 49 ################# */
-// export function isNumber(value: unknown): value is number {
-//   return typeof value === 'number';
-// }
+/* ################# Aula 49 ################# */
+export function isNumber(value: unknown): value is number {
+  return typeof value === 'number';
+}
 
-// export function soma<T>(...args: T[]): number | null {
-//   const retorno = args.reduce((sum, value) => {
-//     if (isNumber(sum) && isNumber(value)) {
-//       return sum + value;
-//     }
-//     return sum;
-//   }, 0);
-//   return retorno;
-// }
+export function soma<T>(...args: T[]): number | null {
+  const retorno = args.reduce((sum, value) => {
+    if (isNumber(sum) && isNumber(value)) {
+      return sum + value;
+    }
+    return sum;
+  }, 0);
+  return retorno;
+}
 
-// console.log(soma(1, 2, 3));
-// console.log(soma(...[1, 2, 3, 'a', 'b', 'c', 1]));
-// console.log(soma('a', 'b', 'c'));
+console.log(soma(1, 2, 3));
+console.log(soma(...[1, 2, 3, 'a', 'b', 'c', 1]));
+console.log(soma('a', 'b', 'c'));
 
-// /* ################# Aula 50 ################# */
-// //Record
-// const objeto1: Record<string, string | number> = {
-//   nome: 'Raul',
-//   sobrenome: 'Gomes',
-//   idade: 30,
-// };
+/* ################# Aula 50 ################# */
+//Record
+const objeto1: Record<string, string | number> = {
+  nome: 'Raul',
+  sobrenome: 'Gomes',
+  idade: 30,
+};
 
-// console.log(objeto1);
+console.log(objeto1);
 
-// type PessoaProtocol = {
-//   nome?: string;
-//   sobrenome?: string;
-//   idade?: number;
-// };
+type PessoaProtocol = {
+  nome?: string;
+  sobrenome?: string;
+  idade?: number;
+};
 
-// //Required ~ Transforma tudo que opcional em obrigatório.
-// type PessoaRequired = Required<PessoaProtocol>;
-// //Required ~ Transforma tudo que obrigatório em opcional.
-// type PessoaPartial = Partial<PessoaRequired>;
-// //Readonly
-// type PessoaReadonly = Readonly<PessoaRequired>;
-// //Pick ~ Escolhe as chaves que eu quero do objeto.
-// type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>;
+//Required ~ Transforma tudo que opcional em obrigatório.
+type PessoaRequired = Required<PessoaProtocol>;
+//Required ~ Transforma tudo que obrigatório em opcional.
+type PessoaPartial = Partial<PessoaRequired>;
+//Readonly
+type PessoaReadonly = Readonly<PessoaRequired>;
+//Pick ~ Escolhe as chaves que eu quero do objeto.
+type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>;
 
-// const objeto2: PessoaReadonly = {
-//   nome: 'Romeu',
-//   sobrenome: 'Carvalho',
-//   idade: 31,
-// };
-// console.log(objeto2);
+const objeto2: PessoaReadonly = {
+  nome: 'Romeu',
+  sobrenome: 'Carvalho',
+  idade: 31,
+};
+console.log(objeto2);
 
-// //Extract e Exclude
-// type ABC = 'A' | 'B' | 'C';
-// type CDE = 'C' | 'D' | 'E';
-// type TipoExclude = Exclude<ABC, CDE>;
-// type TipoExtract = Extract<ABC, CDE>;
+//Extract e Exclude
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+type TipoExclude = Exclude<ABC, CDE>;
+type TipoExtract = Extract<ABC, CDE>;
 
-// //
-// type AccountMongo = {
-//   _id: string;
-//   nome: string;
-//   sobrenome: string;
-//   idade: number;
-// };
+//
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  sobrenome: string;
+  idade: number;
+};
 
-// type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
-//   id: string;
-// };
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  id: string;
+};
 
-// const accountMongo: AccountMongo = {
-//   _id: '06179',
-//   nome: 'Raul',
-//   sobrenome: 'Gomes',
-//   idade: 30,
-// };
+const accountMongo: AccountMongo = {
+  _id: '06179',
+  nome: 'Raul',
+  sobrenome: 'Gomes',
+  idade: 30,
+};
 
-// function mapAccount(accountMongo: AccountMongo): AccountApi {
-//   const { _id, ...accountData } = accountMongo;
-//   return { ...accountData, id: _id };
-// }
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo;
+  return { ...accountData, id: _id };
+}
 
-// const accountApi = mapAccount(accountMongo);
-// console.log(accountApi);
+const accountApi = mapAccount(accountMongo);
+console.log(accountApi);
 
-// //module mode
-// export default 1;
+//module mode
+export default 1;
 
-// /* ################# Exercício ################# */
+/* ################# Exercício ################# */
 /* Qual sua linguagem de programação favorita?
 Python
 JavaScript

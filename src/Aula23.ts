0903d1b6360d5ca355d82d8a1b81c3c63d080432
raul0777 <raul.gome44@gmail.com>
2022-10-05 14:17:05 -0300

    Quando o decorador de classe é chamado

--- src/Aula23.ts
+++ src/Aula23.ts
@@ -1,10 +1,32 @@
-/* ################# Aula 51 ################# */
-@decorator
-export class Animal {
-  constructor(public nome: string, public cor: string) {}
-}
+// /* ################# Aula 51 ################# */
+// @decorator
+// export class Animal {
+//   constructor(public nome: string, public cor: string) {}
+// }
 
-function decorator<T extends new (...args: any[]) => any>(target: T): T {
+// function decorator<T extends new (...args: any[]) => any>(target: T): T {
+//   return class extends target {
+//     cor: string;
+//     nome: string;
+
+//     constructor(...args: any[]) {
+//       super(...args);
+//       this.nome = this.inverte(args[0]);
+//       this.cor = this.inverte(args[1]);
+//     }
+
+//     inverte(valor: string): string {
+//       return valor.split('').reverse().join('');
+//     }
+//   };
+// }
+
+// const animal = new Animal('romeu', 'roxo');
+// console.log(animal);
+
+// /* ################# Aula 52 ################# */
+function inverteNomeCor<T extends new (...args: any[]) => any>(target: T): T {
+  console.log('Sou o decorador e recebi', target);
   return class extends target {
     cor: string;
     nome: string;
@@ -21,5 +43,12 @@ function decorator<T extends new (...args: any[]) => any>(target: T): T {
   };
 }
 
-const animal = new Animal('romeu', 'roxo');
-console.log(animal);
+@inverteNomeCor
+export class Animal {
+  constructor(public nome: string, public cor: string) {
+    console.log('Sou a classe');
+  }
+}
+
+// const animal = new Animal('romeu', 'roxo');
+// console.log(animal);

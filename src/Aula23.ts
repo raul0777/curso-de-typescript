3669925e9eef09ce21d8906b21f391e55371dc08
raul0777 <raul.gome44@gmail.com>
2022-10-05 14:24:57 -0300

    Decorator Factories(Fábrica de decoradores)

--- src/Aula23.ts
+++ src/Aula23.ts
@@ -25,30 +25,61 @@
 // console.log(animal);
 
 // /* ################# Aula 52 ################# */
-function inverteNomeCor<T extends new (...args: any[]) => any>(target: T): T {
-  console.log('Sou o decorador e recebi', target);
-  return class extends target {
-    cor: string;
-    nome: string;
+// function inverteNomeCor<T extends new (...args: any[]) => any>(target: T): T {
+//   console.log('Sou o decorador e recebi', target);
+//   return class extends target {
+//     cor: string;
+//     nome: string;
 
-    constructor(...args: any[]) {
-      super(...args);
-      this.nome = this.inverte(args[0]);
-      this.cor = this.inverte(args[1]);
-    }
+//     constructor(...args: any[]) {
+//       super(...args);
+//       this.nome = this.inverte(args[0]);
+//       this.cor = this.inverte(args[1]);
+//     }
 
-    inverte(valor: string): string {
-      return valor.split('').reverse().join('');
-    }
+//     inverte(valor: string): string {
+//       return valor.split('').reverse().join('');
+//     }
+//   };
+// }
+
+// @inverteNomeCor
+// export class Animal {
+//   constructor(public nome: string, public cor: string) {
+//     console.log('Sou a classe');
+//   }
+// }
+
+// const animal = new Animal('romeu', 'roxo');
+// console.log(animal);
+
+// /* ################# Aula 53 ################# */
+function inverteNomeCor(param1: string, param2: string) {
+  return function <T extends new (...args: any[]) => any>(target: T): T {
+    console.log('Sou o decorador e recebi', target);
+    return class extends target {
+      cor: string;
+      nome: string;
+
+      constructor(...args: any[]) {
+        super(...args);
+        this.nome = this.inverte(args[0]);
+        this.cor = this.inverte(args[1]);
+      }
+
+      inverte(valor: string): string {
+        return valor.split('').reverse().join('') + ' ' + param1 + ' ' + param2;
+      }
+    };
   };
 }
 
-@inverteNomeCor
+@inverteNomeCor('Outra coisa', 'Valor2')
 export class Animal {
   constructor(public nome: string, public cor: string) {
     console.log('Sou a classe');
   }
 }
 
-// const animal = new Animal('romeu', 'roxo');
-// console.log(animal);
+const animal = new Animal('romeu', 'roxo');
+console.log(animal);

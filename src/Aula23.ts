b93831f92946b2d3ddf8d763059df99df2096fe3
raul0777 <raul.gome44@gmail.com>
2022-10-05 15:15:31 -0300

    Decoradores de parâmetro(parameter decorator)

--- src/Aula23.ts
+++ src/Aula23.ts
@@ -94,19 +94,63 @@
 // const animal = new Animal('romeu', 'roxo');
 // console.log(animal);
 // /* ################# Aula 55 ################# */
+// function decorador(
+//   classPrototype: any,
+//   nomeMethod: string,
+//   descriptor: PropertyDescriptor,
+// ): PropertyDescriptor | void {
+//   console.log(classPrototype);
+//   console.log(nomeMethod);
+//   console.log(descriptor);
+//   return {
+//     value: function (...args: string[]) {
+//       return args[0].toUpperCase();
+//     },
+//   };
+// }
+
+// export class UmaPessoa {
+//   nome: string;
+//   sobrenome: string;
+//   idade: number;
+
+//   constructor(nome: string, sobrenome: string, idade: number) {
+//     this.nome = nome;
+//     this.sobrenome = sobrenome;
+//     this.idade = idade;
+//   }
+
+//   @decorador
+//   method(msg: string): string {
+//     return `${this.nome} ${this.sobrenome}: ${msg}`;
+//   }
+
+//   get nomeCompleto(): string {
+//     return this.nome + ' ' + this.sobrenome;
+//   }
+
+//   set nomeCompleto(valor: string) {
+//     const palavras = valor.split(/\s+/g);
+//     const primeiroNome = palavras.shift();
+//     if (!primeiroNome) return;
+//     this.nome = primeiroNome;
+//     this.sobrenome = palavras.join(' ');
+//   }
+// }
+
+// const pessoa = new UmaPessoa('Raul', 'Gomes', 30);
+// const method = pessoa.method('Hello world');
+// console.log(method);
+
+// /* ################# Aula 56 ################# */
 function decorador(
   classPrototype: any,
-  nomeMethod: string,
-  descriptor: PropertyDescriptor,
-): PropertyDescriptor | void {
+  nomeMethod: string | symbol,
+  index: number,
+): any {
   console.log(classPrototype);
   console.log(nomeMethod);
-  console.log(descriptor);
-  return {
-    value: function (...args: string[]) {
-      return args[0].toUpperCase();
-    },
-  };
+  console.log(index);
 }
 
 export class UmaPessoa {
@@ -114,14 +158,17 @@ export class UmaPessoa {
   sobrenome: string;
   idade: number;
 
-  constructor(nome: string, sobrenome: string, idade: number) {
+  constructor(
+    @decorador nome: string,
+    @decorador sobrenome: string,
+    @decorador idade: number,
+  ) {
     this.nome = nome;
     this.sobrenome = sobrenome;
     this.idade = idade;
   }
 
-  @decorador
-  method(msg: string): string {
+  method(@decorador msg: string): string {
     return `${this.nome} ${this.sobrenome}: ${msg}`;
   }
 

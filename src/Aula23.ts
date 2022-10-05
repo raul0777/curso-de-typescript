acd204cb67f38a2e6395d6b9eb9c45fb7816ac6f
raul0777 <raul.gome44@gmail.com>
2022-10-05 14:36:19 -0300

    Composição de decoradores

--- src/Aula23.ts
+++ src/Aula23.ts
@@ -54,8 +54,11 @@
 // console.log(animal);
 
 // /* ################# Aula 53 ################# */
+interface Constructor {
+  new (...args: any[]): any;
+}
 function inverteNomeCor(param1: string, param2: string) {
-  return function <T extends new (...args: any[]) => any>(target: T): T {
+  return function <T extends Constructor>(target: T): T {
     console.log('Sou o decorador e recebi', target);
     return class extends target {
       cor: string;
@@ -74,7 +77,14 @@ function inverteNomeCor(param1: string, param2: string) {
   };
 }
 
-@inverteNomeCor('Outra coisa', 'Valor2')
+function outroDecorador(param1: string) {
+  return function (target: Constructor) {
+    console.log('Sou o outro decorador' + param1);
+    return target;
+  };
+}
+@outroDecorador('O parâmetro do outro decorador') //2
+@inverteNomeCor('Outra coisa', 'Valor2') //1
 export class Animal {
   constructor(public nome: string, public cor: string) {
     console.log('Sou a classe');
